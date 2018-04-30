adcbe702cb1896989929d9856e14e06839da1c31
thetlwinoo <thetlwinoo85@yahoo.com>
2018-05-01 02:26:50 +0800

    Update Country

--- src/services/cities/cities.hooks.js
+++ src/services/cities/cities.hooks.js
@@ -22,11 +22,7 @@ module.exports = {
         stateProvinceID: {
           service: 'general/state-provinces',
           f_key: '_id',
-          l_key: 'stateProvinceID',
-          one: true,
-          query: {
-            $select: ['stateProvinceName']
-          }
+          one: true
         },
          lastEditedBy: {
            service: 'general/people',
