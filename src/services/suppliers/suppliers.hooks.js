6f2299ad0fc58c494e775ee27a0d90c4b954ba87
thetlwinoo <thet.lwin@shaw.com.sg>
2018-05-08 11:02:22 +0800

    Update Code

--- src/services/suppliers/suppliers.hooks.js
+++ src/services/suppliers/suppliers.hooks.js
@@ -1,8 +1,14 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const {
+  authenticate
+} = require('@feathersjs/authentication').hooks;
+const populate = require('feathers-populate-hook');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [
+      authenticate('jwt'),
+      populate.compatibility()
+    ],
     find: [],
     get: [],
     create: [],
@@ -12,7 +18,45 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [
+      populate({
+        supplierCategoryID: {
+          service: 'purchasing/supplier-categories',
+          f_key: '_id',
+          one: true
+        },
+        primaryContactPersonID: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true
+        },
+        alternateContactPersonID: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true
+        },
+        deliveryMethodID: {
+          service: 'general/delivery-methods',
+          f_key: '_id',
+          one: true
+        },
+        deliveryCityID: {
+          service: 'general/cities',
+          f_key: '_id',
+          one: true
+        },
+        postalCityID: {
+          service: 'general/cities',
+          f_key: '_id',
+          one: true
+        },
+        lastEditedBy: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        }
+      })
+    ],
     find: [],
     get: [],
     create: [],
