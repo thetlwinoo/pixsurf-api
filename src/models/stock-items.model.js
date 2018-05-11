2b8bc3669863c12d3550176b089d6dd1b929d389
thetlwinoo <thet.lwin@shaw.com.sg>
2018-05-11 14:07:46 +0800

    Update Limit Image

--- src/models/stock-items.model.js
+++ src/models/stock-items.model.js
@@ -97,7 +97,7 @@ module.exports = function (app) {
       type: String
     },
     photo: {
-      type: String
+      type: Schema.Types.ObjectId
     },
     customFields: Object,
     tags: [String],
