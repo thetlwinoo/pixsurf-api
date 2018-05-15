d6895a224e89aa54489576d3e9028043618d367f
thetlwinoo <thetlwinoo85@yahoo.com>
2018-05-15 09:27:56 +0800

    Update Code

--- src/services/g-drives/index.js
+++ src/services/g-drives/index.js
@@ -1,6 +1,11 @@
 const hooks = require('./hooks');
 const service = require('./service');
+const download = require('./download');
+const exportpdf = require('./export-pdf');
+const list = require('./export-pdf');
+const oauthclient = require('./oauth-client');
+const quickstart = require('./quickstart');
 
-Object.assign(service, { hooks, service });
+Object.assign(service, { hooks, service, download, exportpdf, list, oauthclient, quickstart });
 
 module.exports = service;
