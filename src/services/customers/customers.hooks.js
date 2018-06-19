0fe02cd906263b72b3f3a37a95f3c368ac18c5be
thetlwinoo <thet.lwin@shaw.com.sg>
2018-06-19 12:28:30 +0800

    Update Code

--- src/services/customers/customers.hooks.js
+++ src/services/customers/customers.hooks.js
@@ -1,8 +1,11 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
+const {
+  authenticate
+} = require('@feathersjs/authentication').hooks;
+const populate = require('feathers-populate-hook');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
+    all: [authenticate('jwt'), populate.compatibility()],
     find: [],
     get: [],
     create: [],
@@ -12,7 +15,55 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [
+      populate({
+        person: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        },
+        billToCustomer: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        },
+        customerCategory: {
+          service: 'sales/customer-categories',
+          f_key: '_id',
+          one: true,
+        },
+        buyingGroup: {
+          service: 'sales/buying-groups',
+          f_key: '_id',
+          one: true,
+        },
+        primaryContactPerson: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        },
+        alternateContactPerson: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        },
+        deliveryMethod: {
+          service: 'general/delivery-methods',
+          f_key: '_id',
+          one: true,
+        },
+        deliveryAddress: {
+          service: 'general/addresses',
+          f_key: '_id',
+          one: true,
+        },
+        lastEditedBy: {
+          service: 'general/people',
+          f_key: '_id',
+          one: true,
+        }
+      })
+    ],
     find: [],
     get: [],
     create: [],
