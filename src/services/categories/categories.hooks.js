2011ac45594b1d8d8f64c1f4d438fc66b724f39f
thetlwinoo <thet.lwin@shaw.com.sg>
2018-07-03 12:20:41 +0800

    Update Code

--- src/services/categories/categories.hooks.js
+++ src/services/categories/categories.hooks.js
@@ -3,13 +3,13 @@ const populate = require('feathers-populate-hook');
 
 module.exports = {
   before: {
-    all: [authenticate('jwt')],
+    all: [],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    create: [authenticate('jwt')],
+    update: [authenticate('jwt')],
+    patch: [authenticate('jwt')],
+    remove: [authenticate('jwt')]
   },
 
   after: {
