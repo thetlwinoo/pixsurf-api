fb96d2d715aaa23f08e37e6233d0794f3df21eee
thetlwinoo <thetlwinoo85@yahoo.com>
2018-08-04 23:16:03 +0800

    Update Code

--- src/services/stock-items/stock-items.hooks.js
+++ src/services/stock-items/stock-items.hooks.js
@@ -5,8 +5,8 @@ const populate = require('feathers-populate-hook');
 
 module.exports = {
   before: {
-    all: [      
-      populate.compatibility()      
+    all: [
+      populate.compatibility()
     ],
     find: [],
     get: [],
@@ -23,31 +23,43 @@ module.exports = {
           service: 'purchasing/suppliers',
           f_key: '_id',
           one: true,
-          query:  {
+          query: {
             $select: ['supplierName']
           }
         },
         colorID: {
           service: 'warehouse/colors',
           f_key: '_id',
-          one: true
+          one: true,
+          query: {
+            $select: ['colorName']
+          }
         },
         unitPackageID: {
           service: 'warehouse/package-types',
           f_key: '_id',
-          one: true
+          one: true,
+          query: {
+            $select: ['packageTypeName']
+          }
         },
         outerPackageID: {
           service: 'warehouse/package-types',
           f_key: '_id',
-          one: true
+          one: true,
+          query: {
+            $select: ['packageTypeName']
+          }
         },
         lastEditedBy: {
           service: 'general/people',
           f_key: '_id',
           one: true,
+          query: {
+            $select: ['fullName','preferredName','searchName']
+          }
         }
-      })      
+      })
     ],
     find: [],
     get: [],
