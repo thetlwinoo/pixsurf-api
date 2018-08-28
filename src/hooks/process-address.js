cce957d8e5332b4af38c46132d790714c0ba635e
thetlwinoo <thet.lwin@shaw.com.sg>
2018-08-28 12:32:19 +0800

    Update Code

--- src/hooks/process-address.js
+++ src/hooks/process-address.js
@@ -4,15 +4,34 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    const user = context.params.user;    
-    console.log('Address params',context.params)
+    const {
+      app,
+      method,
+      result,
+      params
+    } = context;
 
-    if (user && context.data) {
-      context.data.person = user._id;
-      context.data.lastEditedBy = user._id;      
+    console.log('params user',params.user)
+    if (params.user) {
+      const response = await app.service('general/people').find({
+        query: {
+          emailAddress: params.user.email
+        }
+      });
+
+      if (response.data.length > 0) {
+        const user = response.data[0];
+        console.log('Address User', user)
+
+        if (user && context.data) {
+          context.data.person = user._id;
+          context.data.lastEditedBy = user._id;
+        }
+      }
+
     }
 
-    console.log('Process Address=>',context.data)
+    console.log('Process Address=>', context.data)
     return context;
   };
 };
