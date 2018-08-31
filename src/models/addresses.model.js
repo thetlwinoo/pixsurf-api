90d2294c2cd30bc5c7153a6dd9bc1385099b727d
thetlwinoo <thet.lwin@shaw.com.sg>
2018-08-31 12:19:32 +0800

    Update Code

--- src/models/addresses.model.js
+++ src/models/addresses.model.js
@@ -16,6 +16,10 @@ module.exports = function (app) {
       type: String,
       required: true
     },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
     addressType: {
       type: Schema.Types.ObjectId,
       required: false
