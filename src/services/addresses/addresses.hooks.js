291f3237b839058d20110f8b1e903c147cfc38bc
thetlwinoo <thet.lwin@shaw.com.sg>
2018-08-31 11:06:46 +0800

    Update Code

--- src/services/addresses/addresses.hooks.js
+++ src/services/addresses/addresses.hooks.js
@@ -13,9 +13,9 @@ module.exports = {
     all: [authenticate('jwt')],
     find: [],
     get: [],
-    create: [],
-    update: [],
-    patch: [],
+    create: [resetDefault()],
+    update: [resetDefault()],
+    patch: [resetDefault()],
     remove: []
   },
 
